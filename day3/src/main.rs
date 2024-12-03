1b7ee80782041f99de91f02f321eb258222e4fa2
finn-wink <winkelmannf70@gmail.com>
2024-12-03 23:30:47 +0100

    day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -21,7 +21,23 @@ fn main() -> io::Result<()> { // returns nothing on success
         }
     }
 
-    
+    let mut step_2_strings: Vec<String> = Vec::new();
+
+    // split the string at the first )
+    for i in 0..3 {
+        if i > 0 {
+            step_1_strings = step_2_strings.clone()
+        }
+        for x in step_1_strings {
+            let split_string: Vec<String> = x
+                .split_inclusive(")")
+                .map(|n| n.trim().to_string())
+                .collect();
+            
+            step_2_strings.push(split_string[0].clone());
+            println!("{}", split_string[0]);
+        } 
+    }
 
     Ok(())
 }
