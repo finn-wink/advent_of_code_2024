e96cea225b30d8c91c7e677a199e9c14d69d8679
finn-wink <winkelmannf70@gmail.com>
2024-12-11 09:21:28 +0100

    day10

--- day10_rs/src/main.rs
+++ day10_rs/src/main.rs
@@ -1,21 +1,25 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use array2d::Array2D;
+use std::collections::HashSet;
 
 fn find_number_positions(matrix: &Array2D<i32>, max_num: i32) -> Vec<Vec<(usize,usize)>> {
 
     let mut all_positions: Vec<Vec<(usize, usize)>> = Vec::new();
-    let mut zero_positions: Vec<(usize, usize)> = Vec::new();
+    let mut num_positions: Vec<(usize, usize)> = Vec::new();
 
     for i in 0..max_num {
+        
+        num_positions.clear();
+        
         for (r, row_iter) in matrix.rows_iter().enumerate() {
-            for (c, element)in row_iter.enumerate() {
+            for (c, element) in row_iter.enumerate() {
                 if element == &i {
-                    zero_positions.push((r as usize, c as usize));
+                    num_positions.push((r as usize, c as usize));
                 }
             }
-            all_positions.push(zero_positions.clone());
         }
+        all_positions.push(num_positions.clone());
     }
     return all_positions
 }
@@ -28,12 +32,11 @@ fn find_next(current: &Vec<(usize, usize)>, next_possible: &Vec<(usize, usize)>)
         for n in next_possible.iter() {
             let diff1 = c.0 as isize - n.0 as isize;
             let diff2 = c.1 as isize - n.1 as isize;
-            println!("{}", diff1);
-            println!("{}", diff2);
+
             if (diff1 == 1 as isize || diff1 == -1 as isize) && diff2 == 0 {
                 new_positions.push(*n);
             }
-            else if (diff2 == 1 as isize|| diff1 == -1 as isize) && diff1 == 0 {
+            else if (diff2 == 1 as isize || diff2 == -1 as isize) && diff1 == 0 {
                 new_positions.push(*n);
             }
         }
@@ -63,31 +66,41 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Make list of lists into 2D array
     let array = Array2D::from_rows(&raw_arr)?;
 
-    let position_list = find_number_positions(&array, 9);
+    // println!("{:?}", array);
+
+    let position_list = find_number_positions(&array, 10); // for loop does not include 9
 
     println!("{:?}", position_list);
 
     let mut cur_search: Vec<(usize, usize)> = Vec::new();
+    let mut total_trails: i32 = 0;
 
-    for (i, current) in position_list.iter().enumerate() {
-        if i == 0 {
-            cur_search = current.clone();
+    for start in position_list[0].iter() {
+
+        for (i, _current) in position_list.iter().enumerate() {
+            if i == 0 {
+                cur_search = vec![start.clone()];
+                println!("{:?}", cur_search);
+            }
+            if i < 9 {
+                let new_search = find_next(&cur_search, &position_list[i+1]);
+                cur_search = new_search;
+            }
         }
-        if i < 8 {
-            let new_search = find_next(&cur_search, &position_list[i+1]);
-            cur_search = new_search;
-        }
+        println!{"{}", cur_search.len()}
+
+        // Find unique tuples
+        let mut seen = HashSet::new();
+    
+        let unique_tops: Vec<_> = cur_search.clone()
+            .into_iter()
+            .filter(|t| seen.insert(*t))
+            .collect();
+    
+        total_trails += unique_tops.len() as i32;
     }
 
-    println!("{:?}", cur_search);
-    
-    // current positions -> next positions 
-
-    // trailheads are at position list[0]
-    // check_list -> first all 0s then all numbers found
-    // Check distance between all combinations
-    // If total distance is 1 or -1 add it to the new list to be checked
-    // Need to find all the total iterations that are run somehow
+    println!("{}", total_trails);
 
     Ok(())
 
